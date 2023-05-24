ca759c56fa246a01d52651da27f3152bb9a62b3c
Ella Marie A. Mallari <elleee.mallari@gmail.com>
2023-05-24 11:19:56 +0800

    File for operations

--- operations.py
+++ operations.py
@@ -1,8 +1,11 @@
 
 import calculator_gui
 
+# class for operations
 class operations():
+    # defining every variables that we need 
     def calc_operations(self, math, entry_num, first_num, second_number):
+        # for addition
         if math == "add":
                 entry_num.insert(0, first_num + int(second_number))
         if math == "subtract":
