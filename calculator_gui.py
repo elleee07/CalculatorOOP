1aaef44cfb0787c109293316072ccd8b5d1df4bd
Ella Marie A. Mallari <elleee.mallari@gmail.com>
2023-05-24 20:02:59 +0800

    Final GUI

--- calculator_gui.py
+++ calculator_gui.py
@@ -147,6 +147,6 @@ except NameError:
     print("Name error located!")
 finally:
     print("\033[96m=" * 130)
-    print("\033[32mCongratulations! Your code is already clear. Thank you :>")
+    print("\033[32m Your code is already clear. Congrats :>")
     print("\033[96m=" * 130)
     root.mainloop()
