864ede2fa2048017c21a01c1be42b465125e0bb3
Ella Marie A. Mallari <elleee.mallari@gmail.com>
2023-05-24 20:01:01 +0800

    OOP Calculator

--- CalculatorOOP.py
+++ CalculatorOOP.py
@@ -4,9 +4,27 @@
 # OOP CALCULATOR
 
 from calcOperations import Operations
+import calculator_gui
 
-# creating class for main calculator 
-class Calculator():
-    def __init__(self):
-        self.result= 0
+# creating class for main calculator
+try:
+    class Calculator():
+        def __init__(self):
+            self.result= 0
 
+# class for operations 
+    class Operations():
+        def calc_calculation():
+            
+            # class for whole use of GUI
+            class calculatorGUI():
+                def __init__(self, root, Calculator, Entry, Tk):
+                    self.calculator = Calculator
+                    self.root = Tk()
+                    root.title("Elle.Calculator")
+                    root.configure(bg="gray")
+except:
+    print("Error found")
+
+finally: 
+    print("No error found.")
--- calcOperations.py
+++ calcOperations.py
@@ -1,13 +1,18 @@
+import calculator_gui
 
 # class for operations
+
 class Operations():
     def calc_calculation(self, math, entry_num, first_num, second_number): 
-
+        # for addition
         if math == "add":
             entry_num.insert(0, first_num + int(second_number))
+        # for subtraction
         if math == "subtract":
             entry_num.insert(0, first_num - int(second_number))
+        # for multiplication
         if math == "multiplication":
             entry_num.insert(0, first_num * int(second_number))
+        # for division 
         if math == "division":
             entry_num.insert(0, first_num / int(second_number))
